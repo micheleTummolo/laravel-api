8a87e73150a97e64ee33d2ffc5a386e6dff29b30
micheleTummolo <tummolomichele02@gmail.com>
2023-03-09 16:26:59 +0100

    first commit

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Type;
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::all();
+
+        return view('admin.projects.index', compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $types = Type::all();
+        return view ('admin.projects.create', compact('types'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreProjectRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProjectRequest $request)
+    {
+        $form_data = $request->validated();
+
+        $slug = Project::generateSlug($request->name);
+
+        $form_data['slug'] = $slug;
+        
+        $newProject = new Project();
+        
+        if($request->hasFile('cover_image')){
+            $path = Storage::disk('public')->put('project_image', $request->cover_image);
+
+            $form_data['cover_image'] = $path;
+        }
+
+        $newProject->fill($form_data);
+
+        $newProject->save();
+
+        return redirect()->route('admin.projects.index')->with('message', 'Progetto aggiunto correttamente');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Project $project)
+    {
+        
+        return view('admin.projects.show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Project $project)
+    {
+        $types = Type::all();
+        return view('admin.projects.edit', compact('project', 'types'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProjectRequest  $request
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProjectRequest $request, Project $project)
+    {
+        $form_data = $request->validated();
+
+        $slug = Project::generateSlug($request->name, '-');
+
+        $form_data['slug'] = $slug;
+
+        if($request->hasFile('cover_image')){
+            if($project->cover_image){
+                Storage::delete($project->cover_image);
+            }
+
+            $path = Storage::disk('public')->put('project_image', $request->cover_image);
+
+            $form_data['cover_image'] = $path;
+        }
+
+        $project->update($form_data);
+
+        return redirect()->route('admin.projects.index')->with('message', $project->name.' è stato modificato correttamente');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Project $project)
+    {
+        $project->delete();
+
+        return redirect()->route('admin.projects.index')->with('message', $project->name.' è stato cancellato correttamente');
+    }
+}
---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Models\Project;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i=0; $i<10; $i++){
+            $newProject = new Project();
+
+            $newProject->name = $faker->sentence(4);
+            $newProject->description = $faker->text(400);
+            $newProject->slug = Str::slug($newProject->name, '-');
+            $newProject->cover_image = $faker->imageUrl(600, 300, 'Projects', true, 'dogs', true, 'jpg');
+
+            $newProject->save();
+        }
+    }
+}
