9f3857a257ae9aed2120fe62e865d4c8721ef0ae
micheleTummolo <tummolomichele02@gmail.com>
2023-05-03 23:55:21 +0200

    Fixed margin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,21 +12,21 @@
     <div class="row">
         <div class="col-5 mt-3">
             <div class="background_dark_2 border_radius_50 table_container">
-                <h3 class="text-white">Progetti inseriti: <span>{{ count($projects) }}</span></h3>
+                <h3 class="text-white m-0">Progetti inseriti: <span>{{ count($projects) }}</span></h3>
             </div>
         </div>
     </div>
     <div class="row">
         <div class="col-5 mt-3">
             <div class="background_dark_2 border_radius_50 table_container">
-                <h3 class="text-white">Tipi inseriti: <span>{{ count($types) }}</span></h3>
+                <h3 class="text-white m-0">Tipi inseriti: <span>{{ count($types) }}</span></h3>
             </div>
         </div>
     </div>
     <div class="row">
         <div class="col-5 mt-3">
             <div class="background_dark_2 border_radius_50 table_container">
-                <h3 class="text-white">Tecnologie inserite: <span>{{ count($technologies) }}</span></h3>
+                <h3 class="text-white m-0">Tecnologie inserite: <span>{{ count($technologies) }}</span></h3>
             </div>
         </div>
     </div>
